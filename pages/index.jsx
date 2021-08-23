a9813948ce213948a117a27601714e3fc594ddb9
Andy Thackray <thackrayaf@gmail.com>
2021-08-23 14:12:48 +0100

    class > className

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,14 +21,14 @@ export default function Home() {
         <Container>
 
             <Card>
-              <div class="card-body">
-                <h3 class="card-title">Using the Overlay Checkout.{' '}</h3>
-                <p class="card-text">Import Head from 'next/head' to add custom elements to the `head` of your page
+              <div className="card-body">
+                <h3 className="card-title">Using the Overlay Checkout.{' '}</h3>
+                <p className="card-text">Import Head from 'next/head' to add custom elements to the `head` of your page
                 with the built-in Head component, and add Paddle JS to the head in a script tag. Using traditional HTML <code>script</code> tags
                 in the <code>head</code> avoids a TypeError present when using <code>Script</code> from <code>next/script</code></p>
-                <p class="card-text">Create a javascript file with your Paddle setup instructions (e.g. environment,
+                <p className="card-text">Create a javascript file with your Paddle setup instructions (e.g. environment,
                 vendor id, callbacks) in /public/js and link to this with a script tag in the page head.</p>
-                <p class="card-text">Define a constant named openCheckout to use as with the onClick event, similar to
+                <p className="card-text">Define a constant named openCheckout to use as with the onClick event, similar to
                 the React implementation in the <Link href="https://developer.paddle.com/guides/how-tos/checkout/paddle-checkout#invoking-the-flexible-checkout">
                 Paddle documentation</Link>.</p> 
                 <p><code>const openCheckout  = () => {'{'}<br></br>
@@ -38,17 +38,17 @@ export default function Home() {
               </div>
             </Card>
             <Card>
-              <div class="card-body">
-                <h3 class="card-title">Using the Inline Checkout.{' '}</h3>
-                <p class="card-text">As with the Overlay Checkout, Import Head from 'next/head' to add custom elements to the 
+              <div className="card-body">
+                <h3 className="card-title">Using the Inline Checkout.{' '}</h3>
+                <p className="card-text">As with the Overlay Checkout, Import Head from 'next/head' to add custom elements to the 
                 `head` of your page with the built-in Head component, and add Paddle JS to the head in a script tag.</p>
-                <p class="card-text">Create a javascript file with your Paddle setup instructions (e.g. environment,
+                <p className="card-text">Create a javascript file with your Paddle setup instructions (e.g. environment,
                 vendor id, callbacks) in /public/js and link to this with a script tag in the page head.</p>
-                <p class="card-text">Create a second javascript file to specify the parameters called with the <code>Paddle.Checkout.open()
+                <p className="card-text">Create a second javascript file to specify the parameters called with the <code>Paddle.Checkout.open()
                 </code> method. You need to pass the parameter <code>method: 'inline'</code> to specify an inline checkout</p>
-                <p class="card-text">Add the div/define the component that you wish to target for the inline checkout. Set the class 
+                <p className="card-text">Add the div/define the component that you wish to target for the inline checkout. Set the class 
                 name to be <code>checkout-container</code>.</p>
-                <p class="card-text">Finally, add a script tag pointing to the javascript file specifying the parameters for <code>Paddle.Checkout.open()
+                <p className="card-text">Finally, add a script tag pointing to the javascript file specifying the parameters for <code>Paddle.Checkout.open()
                 </code>. This should be loaded after your checkout div/component.</p>
                 <Button variant="primary" href="/inline">Go to page</Button>{' '}
               </div>
